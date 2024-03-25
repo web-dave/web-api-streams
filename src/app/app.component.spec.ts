4998ba842aeeb44d20da2fac86357313d603d633
Dave <dave@webdave.de>
2024-03-25 08:57:27 +0100

    renamed project to web-api

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bt' title`, () => {
+  it(`should have the 'web-api' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bt');
+    expect(app.title).toEqual('web-api');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bt');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, web-api'
+    );
   });
 });
